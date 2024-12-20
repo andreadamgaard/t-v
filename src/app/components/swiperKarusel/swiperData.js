e7d6c50b5bcadf296016aad863973038aef49908
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-12-20 02:34:27 +0100

    instal

---
+++ src/app/components/swiperKarusel/swiperData.js
@@ -0,0 +1,8 @@
+export const SwiperData = [
+  { id: "nouveau", link: "/roedvin/", title: "Så er der rødvin!", text: "Det er lækkert", src: "/images/nouveau.webp", alt: "nouveau.webp" },
+  { id: "staffpicks1", link: "/bobler/", title: "BOOOOOBLER MAND!", text: "Lige til din hals!", src: "/images/steffi_shelf.webp", alt: "Staff picks" },
+  { id: "xmas1", link: "/alle-vine/", title: "Se alt vores vin!!", text: "Køb til dig selv!", src: "/images/swiperwines.webp", alt: "CHRISTMAS WINE" },
+  { id: "nouveau-1", link: "/roedvin/", title: "Så er der rødvin!", text: "Det er lækkert", src: "/images/nouveau.webp", alt: "nouveau.webp" },
+  { id: "staffpicks2", link: "/bobler/", title: "BOOOOOBLER MAND!", text: "Lige til din hals!", src: "/images/steffi_shelf.webp", alt: "Staff picks" },
+  { id: "xmas2", link: "/alle-vine/", title: "Se alt vores vin!!", text: "Køb til dig selv!", src: "/images/swiperwines.webp", alt: "CHRISTMAS WINE" },
+];
